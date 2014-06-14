82235fa8676c8373bf3335ffced54fbd2c145636
benjibobs <minecraftmadman621@gmail.com>
2014-06-14 13:50:38 +0100

    Initial commit

---
+++ AdminChat/src/com/github/benjibobsmc/adminchat/Main.java
@@ -0,0 +1,28 @@
+package com.github.benjibobsmc.adminchat;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		// TODO Auto-generated method stub
+		return super.onCommand(sender, command, label, args);
+	}
+	
+}
